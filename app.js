4a1b65e25e2bcab99b660b818f298392b4647f0e
“stevengo497” <ngo.steve497@yahoo.com>
2018-04-05 17:48:02 -0600

    Array created

--- app.js
+++ app.js
@@ -2,100 +2,174 @@ console.log('Up and running!');
 
 $(document).ready(function() {
 
-$('#column1Card1').click(function() {
-    $('#row1Card1').fadeIn("fast");
-});
+let cards = [
+{		name: 'Jackie-Chan',
+		position1: $('#row1Card5'),
+		position2: $('#row3Card2'),
+},
 
-$('#column1Card2').click(function() {
-    $('#row2Card1').fadeIn("fast");
-});
+{		name: 'Jet-Li',
+		position1: $('#row1Card4'),
+		position2: $('#row1Card6')
+},
 
-$('#column1Card3').click(function() {
-    $('#row3Card1').fadeIn("fast");
-});
+{		name: 'Daniel-Wu',
+		position1: $('#row1Card1'),
+		position2: $('#row4Card1')
+},
 
-$('#column1Card4').click(function() {
-    $('#row4Card1').fadeIn("fast");
-});
+{		name: 'Donnie-Yen',
+		position1: $('#row1Card2'),
+		position2: $('#row3Card4')
+},
 
-$('#column2Card1').click(function() {
-    $('#row1Card2').fadeIn("fast");
-});
+{		name: 'Tony-Jaa',
+		position1: $('#row1Card3'),
+		position2: $('#row2Card6')
+},
 
-$('#column2Card2').click(function() {
-    $('#row2Card2').fadeIn("fast");
+{		name: 'Steven-Yeun',
+		position1: $('#row2Card1'),
+		position2: $('#row3Card3')
+},
+
+{		name: 'Steve-Ngo',
+		position1: $('#row2Card2'),
+		position2: $('#row4Card4')
+},
+
+{		name: 'Bruce-Lee',
+		position1: $('#row2Card3'),
+		position2: $('#row3Card5')
+},
+
+{		name: 'Sung-Kang',
+		position1: $('#row2Card4'),
+		position2: $('#row3Card6')
+},
+
+{		name: 'John-Cho',
+		position1: $('#row2Card5'),
+		position2: $('#row4Card5')
+},
+
+{		name: 'Daniel-Dae-Kim',
+		position1: $('#row3Card1'),
+		position2: $('#row4Card3')
+},
+
+{		name: 'Ken-Jeong',
+		position1: $('#row4Card2'),
+		position2: $('#row4Card6')
+},
+
+];
+
+
+
+$('#column5Card1').click(function() {
+    $('#row1Card5').fadeIn("fast");
 });
 
 $('#column2Card3').click(function() {
     $('#row3Card2').fadeIn("fast");
 });
 
-$('#column2Card4').click(function() {
-    $('#row4Card2').fadeIn("fast");
 });
 
-$('#column3Card1').click(function() {
-    $('#row1Card3').fadeIn("fast");
-});
+// $('#column1Card1').click(function() {
+//     $('#row1Card1').fadeIn("fast");
+// });
 
-$('#column3Card2').click(function() {
-    $('#row2Card3').fadeIn("fast");
-});
+// $('#column1Card2').click(function() {
+//     $('#row2Card1').fadeIn("fast");
+// });
 
-$('#column3Card3').click(function() {
-    $('#row3Card3').fadeIn("fast");
-});
+// $('#column1Card3').click(function() {
+//     $('#row3Card1').fadeIn("fast");
+// });
 
-$('#column3Card4').click(function() {
-    $('#row4Card3').fadeIn("fast");
-});
+// $('#column1Card4').click(function() {
+//     $('#row4Card1').fadeIn("fast");
+// });
 
-$('#column4Card1').click(function() {
-    $('#row1Card4').fadeIn("fast");
-});
+// $('#column2Card1').click(function() {
+//     $('#row1Card2').fadeIn("fast");
+// });
 
-$('#column4Card2').click(function() {
-    $('#row2Card4').fadeIn("fast");
-});
+// $('#column2Card2').click(function() {
+//     $('#row2Card2').fadeIn("fast");
+// });
 
-$('#column4Card3').click(function() {
-    $('#row3Card4').fadeIn("fast");
-});
+// $('#column2Card3').click(function() {
+//     $('#row3Card2').fadeIn("fast");
+// });
 
-$('#column4Card4').click(function() {
-    $('#row4Card4').fadeIn("fast");
-});
+// $('#column2Card4').click(function() {
+//     $('#row4Card2').fadeIn("fast");
+// });
 
-$('#column5Card1').click(function() {
-    $('#row1Card5').fadeIn("fast");
-});
+// $('#column3Card1').click(function() {
+//     $('#row1Card3').fadeIn("fast");
+// });
 
-$('#column5Card2').click(function() {
-    $('#row2Card5').fadeIn("fast");
-});
+// $('#column3Card2').click(function() {
+//     $('#row2Card3').fadeIn("fast");
+// });
 
-$('#column5Card3').click(function() {
-    $('#row3Card5').fadeIn("fast");
-});
+// $('#column3Card3').click(function() {
+//     $('#row3Card3').fadeIn("fast");
+// });
 
-$('#column5Card4').click(function() {
-    $('#row4Card5').fadeIn("fast");
-});
+// $('#column3Card4').click(function() {
+//     $('#row4Card3').fadeIn("fast");
+// });
 
-$('#column6Card1').click(function() {
-    $('#row1Card6').fadeIn("fast");
-});
+// $('#column4Card1').click(function() {
+//     $('#row1Card4').fadeIn("fast");
+// });
 
-$('#column6Card2').click(function() {
-    $('#row2Card6').fadeIn("fast");
-});
+// $('#column4Card2').click(function() {
+//     $('#row2Card4').fadeIn("fast");
+// });
 
-$('#column6Card3').click(function() {
-    $('#row3Card6').fadeIn("fast");
-});
+// $('#column4Card3').click(function() {
+//     $('#row3Card4').fadeIn("fast");
+// });
 
-$('#column6Card4').click(function() {
-    $('#row4Card6').fadeIn("fast");
-});
+// $('#column4Card4').click(function() {
+//     $('#row4Card4').fadeIn("fast");
+// });
 
-});
+// $('#column5Card1').click(function() {
+//     $('#row1Card5').fadeIn("fast");
+// });
+
+// $('#column5Card2').click(function() {
+//     $('#row2Card5').fadeIn("fast");
+// });
+
+// $('#column5Card3').click(function() {
+//     $('#row3Card5').fadeIn("fast");
+// });
+
+// $('#column5Card4').click(function() {
+//     $('#row4Card5').fadeIn("fast");
+// });
+
+// $('#column6Card1').click(function() {
+//     $('#row1Card6').fadeIn("fast");
+// });
+
+// $('#column6Card2').click(function() {
+//     $('#row2Card6').fadeIn("fast");
+// });
+
+// $('#column6Card3').click(function() {
+//     $('#row3Card6').fadeIn("fast");
+// });
+
+// $('#column6Card4').click(function() {
+//     $('#row4Card6').fadeIn("fast");
+// });
+
