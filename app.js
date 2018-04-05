1f32747102f38338a451da524a92ba46ce669dab
“stevengo497” <ngo.steve497@yahoo.com>
2018-04-05 17:19:25 -0600

    all cards are flipped

--- app.js
+++ app.js
@@ -1,1 +1,101 @@
 console.log('Up and running!');
+
+$(document).ready(function() {
+
+$('#column1Card1').click(function() {
+    $('#row1Card1').fadeIn("fast");
+});
+
+$('#column1Card2').click(function() {
+    $('#row2Card1').fadeIn("fast");
+});
+
+$('#column1Card3').click(function() {
+    $('#row3Card1').fadeIn("fast");
+});
+
+$('#column1Card4').click(function() {
+    $('#row4Card1').fadeIn("fast");
+});
+
+$('#column2Card1').click(function() {
+    $('#row1Card2').fadeIn("fast");
+});
+
+$('#column2Card2').click(function() {
+    $('#row2Card2').fadeIn("fast");
+});
+
+$('#column2Card3').click(function() {
+    $('#row3Card2').fadeIn("fast");
+});
+
+$('#column2Card4').click(function() {
+    $('#row4Card2').fadeIn("fast");
+});
+
+$('#column3Card1').click(function() {
+    $('#row1Card3').fadeIn("fast");
+});
+
+$('#column3Card2').click(function() {
+    $('#row2Card3').fadeIn("fast");
+});
+
+$('#column3Card3').click(function() {
+    $('#row3Card3').fadeIn("fast");
+});
+
+$('#column3Card4').click(function() {
+    $('#row4Card3').fadeIn("fast");
+});
+
+$('#column4Card1').click(function() {
+    $('#row1Card4').fadeIn("fast");
+});
+
+$('#column4Card2').click(function() {
+    $('#row2Card4').fadeIn("fast");
+});
+
+$('#column4Card3').click(function() {
+    $('#row3Card4').fadeIn("fast");
+});
+
+$('#column4Card4').click(function() {
+    $('#row4Card4').fadeIn("fast");
+});
+
+$('#column5Card1').click(function() {
+    $('#row1Card5').fadeIn("fast");
+});
+
+$('#column5Card2').click(function() {
+    $('#row2Card5').fadeIn("fast");
+});
+
+$('#column5Card3').click(function() {
+    $('#row3Card5').fadeIn("fast");
+});
+
+$('#column5Card4').click(function() {
+    $('#row4Card5').fadeIn("fast");
+});
+
+$('#column6Card1').click(function() {
+    $('#row1Card6').fadeIn("fast");
+});
+
+$('#column6Card2').click(function() {
+    $('#row2Card6').fadeIn("fast");
+});
+
+$('#column6Card3').click(function() {
+    $('#row3Card6').fadeIn("fast");
+});
+
+$('#column6Card4').click(function() {
+    $('#row4Card6').fadeIn("fast");
+});
+
+});
